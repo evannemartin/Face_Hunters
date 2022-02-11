a88f62946860d8f17ba1109e0423a2e69967af73
evannemartin <evanne.martin@insa-lyon.fr>
2022-02-11 16:48:51 +0100

    Modified the doctring for a google style + test and created __init__.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 def sum(a,b):
-  """ return the sum of the 2 input a and b. """
+    """
+    return the sum of the 2 input a and b.
+
+    Args :
+        a (float): nb1
+        b (float) : nb2
+
+    Returns :
+        float : sum
+
+    Examples :
+    >>>sum(1,3)
+    4
+    >>>sum(40,10)
+    50
+
+    """
+
   return a+b
 
 print(sum(2,3))
