b2d23c691d042d2c97fc69933176b4b690c775c2
meriem <meriem-chorfi.0022@hotmail.com>
2022-03-14 10:33:52 +0100

    rajout encodeur

--- Project/AutoEncoder.py
+++ Project/AutoEncoder.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import os
+from glob import glob
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Input, Conv2D, Flatten, Dense, Conv2DTranspose, Reshape, Lambda, Activation, BatchNormalization, LeakyReLU, Dropout
 from keras.models import Model
@@ -8,7 +9,7 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from keras.utils import plot_model
 
-# PARCOURT DE LA BASE DE DONNÉES :
+# DATASET :
 DATA_FOLDER = './database/img_align_celeba/img_align_celeba'
 filenames = np.array(glob(os.path.join(DATA_FOLDER, '*/*.jpg')))
 NUM_IMAGES = len(filenames)
@@ -27,4 +28,62 @@ data_flow = ImageDataGenerator(rescale=1./255).flow_from_directory(DATA_FOLDER,
                                                                    )
 
 
-                                                                 
+# BUILDING THE ENCODER :
+def build_vae_encoder(input_dim, output_dim, conv_filters, conv_kernel_size,
+                  conv_strides, use_batch_norm = False, use_dropout = False):
+
+  # Clear tensorflow session to reset layer index numbers to 0 for LeakyRelu,
+  # BatchNormalization and Dropout.
+  # Otherwise, the names of above mentioned layers in the model
+  # would be inconsistent
+  global K
+  K.clear_session()
+
+  # Number of Conv layers
+  n_layers = len(conv_filters)
+
+  # Define model input
+  encoder_input = Input(shape = input_dim, name = 'encoder_input')
+  x = encoder_input
+
+  # Add convolutional layers
+  for i in range(n_layers):
+      x = Conv2D(filters = conv_filters[i],
+                  kernel_size = conv_kernel_size[i],
+                  strides = conv_strides[i],
+                  padding = 'same',
+                  name = 'encoder_conv_' + str(i)
+                  )(x)
+      if use_batch_norm:
+        x = BathcNormalization()(x)
+
+      x = LeakyReLU()(x)
+
+      if use_dropout:
+        x = Dropout(rate=0.25)(x)
+
+  # Required for reshaping latent vector while building Decoder
+  shape_before_flattening = K.int_shape(x)[1:]
+
+  x = Flatten()(x)
+
+  mean_mu = Dense(output_dim, name = 'mu')(x)
+  log_var = Dense(output_dim, name = 'log_var')(x)
+
+  # Defining a function for sampling
+  def sampling(args):
+    mean_mu, log_var = args
+    epsilon = K.random_normal(shape=K.shape(mean_mu), mean=0., stddev=1.)
+    return mean_mu + K.exp(log_var/2)*epsilon
+
+  # Using a Keras Lambda Layer to include the sampling function as a layer
+  # in the model
+  encoder_output = Lambda(sampling, name='encoder_output')([mean_mu, log_var])
+
+  return encoder_input, encoder_output, mean_mu, log_var, shape_before_flattening, Model(encoder_input, encoder_output)
+
+vae_encoder_input, vae_encoder_output,  mean_mu, log_var, vae_shape_before_flattening, vae_encoder  = build_vae_encoder(input_dim = INPUT_DIM,
+                                    output_dim = Z_DIM,
+                                    conv_filters = [32, 64, 64, 64],
+                                    conv_kernel_size = [3,3,3,3],
+                                    conv_strides = [2,2,2,2])
