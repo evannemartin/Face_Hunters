c10dcbfd202863387a92a9739c2cc1b295f74478
meriem <meriem-chorfi.0022@hotmail.com>
2022-03-14 20:23:43 +0100

    changement total du autoencodeur

--- Project/AutoEncoder.py
+++ Project/AutoEncoder.py
@@ -1,14 +1,15 @@
-import numpy as np
+ import numpy as np                   # advanced math library
+import matplotlib.pyplot as plt      # plotting routines
+from keras.models import Model       # Model type to be used
+from keras.layers.core import Dense, Dropout, Activation # Types of layers to be used in our model
+from keras.utils import np_utils                         # NumPy related tools
+import keras
+import tensorflow as tf
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from glob import glob
 from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import Input, Conv2D, Flatten, Dense, Conv2DTranspose, Reshape, Lambda, Activation, BatchNormalization, LeakyReLU, Dropout
-from keras.models import Model
-from keras import backend as K
-from tensorflow.keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint
-from keras.utils.vis_utils import plot_model
+
 
 # DATASET :
 DATA_FOLDER = './database/img_align_celeba'
@@ -20,7 +21,7 @@ INPUT_DIM = (128,128,3) # Image dimension
 BATCH_SIZE = 512
 Z_DIM = 200 # Dimension of the latent vector (z)
 
-data_flow = ImageDataGenerator(rescale=1./255).flow_from_directory(DATA_FOLDER,
+X = ImageDataGenerator(rescale=1./255).flow_from_directory(DATA_FOLDER,
                                                                    target_size = INPUT_DIM[:2],
                                                                    batch_size = BATCH_SIZE,
                                                                    shuffle = True,
@@ -29,65 +30,77 @@ data_flow = ImageDataGenerator(rescale=1./255).flow_from_directory(DATA_FOLDER,
                                                                    )
 
 
-# BUILDING THE ENCODER :
-def build_vae_encoder(input_dim, output_dim, conv_filters, conv_kernel_size,
-                  conv_strides, use_batch_norm = False, use_dropout = False):
+#split the database into training and testing
+from sklearn.model_selection import train_test_split
+X_train, X_test = train_test_split(X, test_size=0.2, random_state=0)
 
-  # Clear tensorflow session to reset layer index numbers to 0 for LeakyRelu,
-  # BatchNormalization and Dropout.
-  # Otherwise, the names of above mentioned layers in the model
-  # would be inconsistent
-  global K
-  K.clear_session()
+#building the encoder and decoder
 
-  # Number of Conv layers
-  n_layers = len(conv_filters)
-
-  # Define model input
-  encoder_input = Input(shape = input_dim, name = 'encoder_input')
-  x = encoder_input
-
-  # Add convolutional layers
-  for i in range(n_layers):
-      x = Conv2D(filters = conv_filters[i],
-                  kernel_size = conv_kernel_size[i],
-                  strides = conv_strides[i],
-                  padding = 'same',
-                  name = 'encoder_conv_' + str(i)
-                  )(x)
-      if use_batch_norm:
-        x = BathcNormalization()(x)
-
-      x = LeakyReLU()(x)
-
-      if use_dropout:
-        x = Dropout(rate=0.25)(x)
-
-  # Required for reshaping latent vector while building Decoder
-  shape_before_flattening = K.int_shape(x)[1:]
-
-  x = Flatten()(x)
-
-  mean_mu = Dense(output_dim, name = 'mu')(x)
-  log_var = Dense(output_dim, name = 'log_var')(x)
-
-  # Defining a function for sampling
-  def sampling(args):
-    mean_mu, log_var = args
-    epsilon = K.random_normal(shape=K.shape(mean_mu), mean=0., stddev=1.)
-    return mean_mu + K.exp(log_var/2)*epsilon
-
-  # Using a Keras Lambda Layer to include the sampling function as a layer
-  # in the model
-  encoder_output = Lambda(sampling, name='encoder_output')([mean_mu, log_var])
-
-  return encoder_input, encoder_output, mean_mu, log_var, shape_before_flattening, Model(encoder_input, encoder_output)
+original_dim = X.shape[1]
+hidden_encoding_dim = 512
+encoding_dim = 64
+hidden_decoding_dim = 512
+dropout_level = 0.1
 
 
-# testing the encoder :
-vae_encoder_input, vae_encoder_output,  mean_mu, log_var, vae_shape_before_flattening, vae_encoder  = build_vae_encoder(input_dim = INPUT_DIM,
-                                    output_dim = Z_DIM,
-                                    conv_filters = [32, 64, 64, 64],
-                                    conv_kernel_size = [3,3,3,3],
-                                    conv_strides = [2,2,2,2])
-print(vae_encoder_input, vae_encoder_output, mean_mu, log_var, vae_shape_before_flattening, vae_encoder)
+# Definition of the model that will contain a encoder and a decoder:
+# "encoded" is the encoded representation of the input
+
+def model(original_dim, hidden_encoding_dim, encoding_dim,
+           dropout_level, hidden_decoding_dim):
+           """
+
+           """
+    input_img = keras.Input(shape=(original_dim,))
+    hidden_encoded = Dense(hidden_encoding_dim, activation='relu')(input_img)
+    dropout_hidden_encoded = Dropout(dropout_level)(hidden_encoded)
+    encoded = Dense(encoding_dim, activation='relu')(dropout_hidden_encoded)
+    dropout_encoded = Dropout(dropout_level)(encoded)
+
+    # "decoded" is the reconstruction of the input
+    hidden_decoded = Dense(hidden_decoding_dim, activation='relu')(dropout_encoded)
+    dropout_hidden_decoded = Dropout(dropout_level)(hidden_decoded)
+    decoded = Dense(original_dim, activation='sigmoid')(dropout_hidden_decoded)
+
+    # This model maps an input to its reconstruction
+    autoencoder = keras.Model(input_img, decoded)
+    # This model maps an input to is encoded representation
+    encoder = keras.Model(input_img, encoded)
+    # This model maps an imput with the same dim as the encoded to the reconstruction
+    input_encoded_img = keras.Input(shape=(encoding_dim,))
+    hidden_decoder_layer = autoencoder.layers[-3]
+    hidden_dropout_decoded_layer = autoencoder.layers[-2]
+    decoder_layer = autoencoder.layers[-1]
+    decoder = keras.Model(input_encoded_img, decoder_layer(hidden_dropout_decoded_layer(hidden_decoder_layer(input_encoded_img))))
+
+    return encoder, decoder, autoencoder
+
+
+# compile the model
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+
+# Definition of the method plot
+
+encoded_imgs = encoder.predict(X_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+# Use Matplotlib (don't ask)
+import matplotlib.pyplot as plt
+
+n = 10  # How many faces we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # Display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(X_test[i].reshape(64, 64))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # Display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(64, 64))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
