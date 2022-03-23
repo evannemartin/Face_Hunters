5c2a91bdaccc4e97dc7689a9768da03cdac0c143
meriem <meriem-chorfi.0022@hotmail.com>
2022-03-23 20:19:36 +0100

    encoded version finale merieme mercredi

--- Project/AutoEncoder.py
+++ Project/AutoEncoder.py
@@ -1,225 +1,124 @@
-import numpy as np                   # advanced math library
-import matplotlib.pyplot as plt      # plotting routines
-from keras.models import Model       # Model type to be used
-from keras.layers.core import Dense, Dropout, Activation # Types of layers to be used in our model
-from keras.utils import np_utils                         # NumPy related tools
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Model
+from keras.layers.core import Dense, Dropout, Activation
+from keras.utils import np_utils
 import keras
 import tensorflow as tf
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import array_to_img
+from multiprocessing import Pool
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import PIL
-from glob import glob
-from keras.preprocessing.image import ImageDataGenerator
+from PIL import Image
+from matplotlib import image
+
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose
+
+# PART 1
+# In this code, we need to work with numpys. To do so, we need to convert our database
+# To an numpy array.
+# UPLOAD THE PATH OF THE DATABASE:
+data_path="./database/img_align_celeba_500"
+listing = os.listdir(data_path)
+print(listing) #returns a list of all the files of the path
+listarray = [] # creating the array list that will contain the information of our images
+
+def cut_list(list, length):
+    listing_parts = []
+    intervalle_0 = 0
+    intervalle_1 = length
+    while intervalle_0 <=(len(list)):
+        listing_parts.append(list[intervalle_0:intervalle_1])
+        intervalle_0 = intervalle_1
+        intervalle_1 = intervalle_1 + length
+    return listing_parts
+
+# we choose first to work with only 500 images.
+listing_parts=cut_list(listing,500)
+
+#Once we have uploaded all our images, we resize our images and fit them in numpy array
+from skimage.transform import resize
+for file in listing_parts[0]:
+        if file == file + '.DS_Store':
+            continue
+        chemin= "./database/img_align_celeba_500/" + file
+        im = image.imread(chemin)
+        resized_img = resize(im,(128,128))
+        listarray.append(resized_img)
+nparray = np.array(listarray)
 
 
-# Definition of the model that will contain a encoder and a decoder:
+# PART 2:  THE ENCODER
+#we construct our encoder :
+from sklearn.model_selection import train_test_split
+X_train, X_test = train_test_split (nparray, test_size=0.2, random_state=0)
 
-def model(original_dim, hidden_encoding_dim, encoding_dim, dropout_level, hidden_decoding_dim):
-    """
-         This method contains an encoder and a decoder (neuronal model)
-         The encoder program takes an image as a parameter and returns a low dimensional vector
-         The decoder program takes the vector as a parameter and learns to reconstruct an image from this one.
+input_layer = Input(shape=(128, 128, 3), name="INPUT")
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_layer)
+x = MaxPooling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
 
-         args :
-          original_dim (int)
-          hidden_encoding_dim (int)
-          encoding_dim (int)
-          dropout_level (int)
-          hidden_decoding_dim (int)
+code_layer = MaxPooling2D((2, 2), name="CODE")(x)
 
-        returns :
-          (keras.engine.functional.Functional) : encoder
-          (keras.engine.functional.Functional) : decoder
-          (keras.engine.functional.Functional) : autoencoder
-         """
-    input_img = keras.Input(shape=(original_dim,))
-    hidden_encoded = Dense(hidden_encoding_dim, activation='relu')(input_img)
-    dropout_hidden_encoded = Dropout(dropout_level)(hidden_encoded)
-    encoded = Dense(encoding_dim, activation='relu')(dropout_hidden_encoded)
-    dropout_encoded = Dropout(dropout_level)(encoded)
+x = Conv2DTranspose(8, (3, 3), activation='relu', padding='same')(code_layer)
+x = UpSampling2D((2, 2))(x)
+x = Conv2DTranspose(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2DTranspose(16, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2,2))(x)
+output_layer = Conv2D(3, (3, 3), padding='same', name="OUTPUT")(x)
 
-    # "decoded" is the reconstruction of the input
-    hidden_decoded = Dense(hidden_decoding_dim, activation='relu')(dropout_encoded)
-    dropout_hidden_decoded = Dropout(dropout_level)(hidden_decoded)
-    decoded = Dense(original_dim, activation='sigmoid')(dropout_hidden_decoded)
-
-    # This model maps an input to its reconstruction
-    autoencoder = keras.Model(input_img, decoded)
-
-    # This model maps an input to is encoded representation
-    encoder = keras.Model(input_img, encoded)
-
-    # This model maps an imput with the same dim as the encoded to the reconstruction
-    input_encoded_img = keras.Input(shape=(encoding_dim,))
-    hidden_decoder_layer = autoencoder.layers[-3]
-    hidden_dropout_decoded_layer = autoencoder.layers[-2]
-    decoder_layer = autoencoder.layers[-1]
-    decoder = keras.Model(input_encoded_img, decoder_layer(hidden_dropout_decoded_layer(hidden_decoder_layer(input_encoded_img))))
-
-    return encoder, decoder, autoencoder
+AE = Model(input_layer, output_layer)
+AE.compile(optimizer='adam', loss='mse')
 
 
-# Definition of the method plot
-def plot_images(n,decoded):
-    """
-    This method plots the n images before and after the reconstruction of the autoencoder
-    args :
-        decoded (array)
-        n (int) : number of faces we will display
-    returns :
-        None
+# We train the encoder
+AE.fit(X_train, X_train, epochs=2, batch_size=32, shuffle=True, validation_data=(X_test, X_test))
 
-    """
-    encoded_imgs = encoder.predict(X_test)
-    decoded_imgs = decoder.predict(encoded_imgs)
-    import matplotlib.pyplot as plt
+# We need now to obtain the encoded vector that will be used for the genetic algorithms part:
+def auto_encoder():
+    get_encoded_X = Model(inputs=AE.input, outputs=AE.get_layer("CODE").output)
 
-    n = 10  # How many faces we will display
-    plt.figure(figsize=(20, 4))
+    encoded = get_encoded_X.predict(X_test)
+    encoded = encoded.reshape((len(X_test), 16*16*8))
+
+    reconstructed = AE.predict(X_test)
+    return encoded, reconstructed
+
+encoded, reconstructed=auto_encoder()
+np.savetxt("./vecteur.txt", encoded) # THE ENCODED VECTOR IS HERE, A txt file is given. to use it for the genetic algorithm
+# you need to reupload it ;)
+
+
+# PART 3:  THE DECODER
+
+input_layer_decodeur = Input(shape=(16,16,8), name="INPUT")
+x_decodeur = Conv2DTranspose(8, (3, 3), activation='relu', padding='same')(input_layer_decodeur)
+x_decodeur = UpSampling2D((2, 2))(input_layer_decodeur)
+x_decodeur = Conv2DTranspose(8, (3, 3), activation='relu', padding='same')(input_layer_decodeur)
+x_decodeur = UpSampling2D((2, 2))(input_layer_decodeur)
+x_decodeur = Conv2DTranspose(16, (3, 3), activation='relu', padding='same')(input_layer_decodeur)
+x_decodeur = UpSampling2D((2,2))(input_layer_decodeur)
+output_layer_decodeur = Conv2D(3, (3, 3), padding='same', name="OUTPUT")(input_layer_decodeur)
+
+D = Model(input_layer_decodeur,output_layer_decodeur)
+D.compile(optimizer='adam', loss='mse')
+
+
+# PART 4: PLOTTING THE PICTURES
+
+def show_face_data(nparray, n=10, title=""):
+    plt.figure(figsize=(30, 5))
     for i in range(n):
-        # Display original
-        ax = plt.subplot(2, n, i + 1)
-        plt.imshow(X_test[i].reshape(128, 128))
-        plt.gray()
+        ax = plt.subplot(2,n,i+1)
+        plt.imshow(array_to_img(nparray[i]))
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
-
-        # Display reconstruction
-        ax = plt.subplot(2, n, i + 1 + n)
-        plt.imshow(decoded_imgs[i].reshape(128, 128))
-        plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
+    plt.suptitle(title, fontsize = 20)
     plt.show()
-    return None
 
-# LOAD the DATASET (only 504 first pictures) :
-DATA_FOLDER="./database/img_align_celeba"
-"""X=[]
-for i in range(1,10): #501 so that it doesn't crash for now
-    img = PIL.Image.open(DATA_FOLDER+"00000"+str(i)+".jpg") # This returns an image object
-    img = np.asarray(img) # convert it to ndarray
-    print(type(img))
-    X.append(X)
-"""
-filenames = np.array(glob(os.path.join(DATA_FOLDER, '*/*.jpg')))
-NUM_IMAGES = len(filenames)
-#print("Total number of images : " + str(NUM_IMAGES))
-
-INPUT_DIM = (128,128,3) # Image dimension
-BATCH_SIZE = 500
-data_flow = ImageDataGenerator(rescale=1./255).flow_from_directory(DATA_FOLDER,
-                                                                   target_size = INPUT_DIM[:2],
-                                                                   batch_size = BATCH_SIZE,
-                                                                   shuffle = True,
-                                                                   class_mode = 'input',
-                                                                   subset = 'training'
-
-# try to upload the images in an other way                                                                    )
-#print(data_flow[1])
-#print(data_flow.__len__())
-"""X=np.empty(504,dtype=object)
-for i in range(len(data_flow)):
-    X[i]=data_flow[i]"""
-
-
-#print(X[1])
-#X=np.asarray(X)
-#print(np.shape(X[0]))
-
-# TEST THE DIFFERENT FUNCTIONS
-
-# first, we need to split the database into training and testing
-#from sklearn.model_selection import train_test_split
-#X_train, X_test = train_test_split(X, test_size=0.2, random_state=0)
-
-''''
-#Construct:
-original_dim = 128*128
-hidden_encoding_dim = 512
-encoding_dim = 64
-hidden_decoding_dim = 512
-dropout_level = 0.1
-(encoder_, decoder_, autoencoder_) = model(original_dim, hidden_encoding_dim, encoding_dim, dropout_level, hidden_decoding_dim)
-
-# Compile the model
-autoencoder_.compile(optimizer='adam', loss='binary_crossentropy')
-
-# Fit the model
-autoencoder_.fit(X_train, X_train,
-                epochs=300,
-                batch_size=64,
-                shuffle=True,
-                validation_data=(X_test, X_test))
-
-# Plot reconstruction
-encoded_imgs = encoder_.predict(X_test)
-decoded_imgs = decoder_.predict(encoded_imgs)
-N = 2  # How many faces we will display
-plot_images(1, decoded_imgs)
-plot_images(9, decoded_imgs)
-''''
-
-input_shape = (128,128,3)
-encoded_dim = 1000
-
-input_img = keras.Input(shape=input_shape)
-x = keras.layers.Conv2D(128, (3, 3),strides=1,activation='relu', padding='same')(input_img)
-x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-x = keras.layers.Dropout(0.2)(x)
-x = keras.layers.Conv2D(64, (3, 3),strides=1,activation='relu', padding='same')(x)
-x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-x = keras.layers.Dropout(0.2)(x)
-x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-x = keras.layers.Dropout(0.2)(x)
-x = keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-x = keras.layers.Dropout(0.2)(x)
-x = keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-x = keras.layers.Dropout(0.2)(x)
-x = keras.layers.Flatten()(x)
-x = keras.layers.Dense(128,"relu")(x)
-encoded = keras.layers.Dense(encoded_dim,activation='relu')(x)
-
-# at this point the representation is (4, 4, 8) i.e. 128-dimensional
-x = keras.layers.Dense(128,"relu")(encoded)
-x = keras.layers.Dense(4*4*8,"relu")(x)
-x = keras.layers.Reshape((4,4,8))(x)
-x = keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = keras.layers.UpSampling2D((2, 2))(x)
-x = keras.layers.Dropout(0.2)(x)
-x = keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-x = keras.layers.UpSampling2D((2, 2))(x)
-x = keras.layers.Dropout(0.2)(x)
-x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-x = keras.layers.UpSampling2D((2, 2))(x)
-x = keras.layers.Dropout(0.2)(x)
-x = keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-x = keras.layers.UpSampling2D((2, 2))(x)
-x = keras.layers.Dropout(0.2)(x)
-x = keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same')(x)
-x = keras.layers.UpSampling2D((2, 2))(x)
-decoded = keras.layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
-
-autoencoder = keras.Model(input_img, decoded)
-encoder = keras.Model(input_img, encoded)
-autoencoder.compile(optimizer='adam', loss='categorical_crossentropy')
-
-#X_train = X_train.reshape(-1,128,128,3)
-#X_test = X_test.reshape(-1,128,128,3)
-"""autoencoder.fit(X_train, X_train,
-                epochs=100,
-                batch_size=32,
-                shuffle=True,
-                validation_data=(X_test, X_test))"""
-N_EPOCHS = 100
-autoencoder.fit_generator(data_flow,
-                        shuffle=True,
-                        epochs = N_EPOCHS,
-                        initial_epoch = 0,
-                        steps_per_epoch=NUM_IMAGES / BATCH_SIZE)
-#encoder.predict(X)   #To get the activations
-
-#decoder.predict()    #To get generate the new faces
+show_face_data(X_test, title="original faces")
+show_face_data(reconstructed, title="reconstructed faces")
